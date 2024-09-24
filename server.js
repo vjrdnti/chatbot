6d934ad69d4eee3a07011b810c08cc99554e4120
vjrdnti <shishir5903tripathi@gmail.com>
2024-09-24 11:45:44 +0530

    added secrets and deployment testing

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const connectDB = require('./config/db');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-//const dotenv = require('dotenv');
+const dotenv = require('dotenv');
 
 // Load environment variables
-//dotenv.config();
+dotenv.config();
 
 const MONGO_URI = process.env.MONGO_URI; // Will be populated by the secret in GitHub Actions or your local .env file
 const JWT_SECRET = process.env.JWT_SECRET;
